16a98c7bd1bfec4ac3e8ee39dd8f86fbea02cf7e
Jessie Alegado <jessiealegado21@gmai.com>
2026-04-27 23:55:58 +0800

    second commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios, { type AxiosInstance } from 'axios'
 const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL
     ? `${import.meta.env.VITE_API_BASE_URL}/api`
-    : 'retailchaindb.database.windows.net',
+    : 'https://retailchain-api-2384.azurewebsites.net',
 })
 
 // Attach JWT token automatically to every request
